ee53d049015866f428c5acc9e3316c7a8c3d9f6b
Alicia <ramtinealicia@gmail.com>
2025-09-22 17:11:31 +0200

    Avances lista dinamica imagenes

--- Models/Boligrafo.cs
+++ Models/Boligrafo.cs
@@ -4,4 +4,9 @@ public class Boligrafo
 {
     public string Codigo { get; set; }
     public string Color {get; set;}
+
+    public override string ToString()
+    {
+        return "Codigo: "+Codigo + " Color: " + Color;
+    }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Bindings.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -8,15 +9,44 @@ namespace Bindings.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    //decorador, privado, minuscula, sin get ni set
+    //decorador, privado, minuscula, sin get ni set (el observavbleproperty los implementa automaticamnete)
     [ObservableProperty]
     private string mensaje = string.Empty;
+
+    [ObservableProperty] private bool avanzadas = false;
+
+    [ObservableProperty] private List<Boligrafo> boligrafos= new();
+    
+    
     public string Greeting { get; } = "Welcome to Ocaña!";
     public string Saludo { set; get; } = "Saludos🛐";
+    
+    [ObservableProperty]
+    private Boligrafo boli = new Boligrafo();
 
     public MainWindowViewModel()
     {
         CargarCombo();
+        CargarBolis();
+    }
+    
+
+    private void CargarBolis()
+    {
+        Boligrafo boli = new Boligrafo();
+        boli.Codigo = "1";
+        boli.Color = "Rosa";
+        Boligrafos.Add(boli);
+        
+        Boligrafo boli2 = new Boligrafo();
+        boli2.Codigo = "2";
+        boli2.Color = "Perla";
+        Boligrafos.Add(boli2);
+        
+        Boligrafo boli3 = new Boligrafo();
+        boli3.Codigo = "3";
+        boli3.Color = "Fucsia";
+        Boligrafos.Add(boli3);
     }
 
     public ObservableCollection<string> Lista { set; get; } = new()
@@ -33,26 +63,43 @@ public partial class MainWindowViewModel : ViewModelBase
         Boli.Color = Lista[0];
     }
     
-    public Boligrafo Boli { get; set; } = new Boligrafo();
+    
 
     [RelayCommand]
     public void MostrarBoli(object parameter)
     {
         if (parameter is false)
         {
-            Mensaje = "eres imbecil";
-            Console.WriteLine("eres imbecil");
+            Mensaje = "Marcar el check";
+            Console.WriteLine("Marcar el chek");
             
             return;
-        }
-
-        if (string.IsNullOrWhiteSpace(Boli.Codigo))
+        }else if(string.IsNullOrWhiteSpace(Boli.Codigo))
         {
-            Mensaje = "eres super imbecil";
-            Console.WriteLine("eres super imbecil");
+            Mensaje = "Codigo null";
+            Console.WriteLine("Codigo null");
             
         }
+        else
+        {
+            Mensaje = Boli.Codigo+" "+Boli.Color;
+            Console.WriteLine(Boli.Codigo+" "+Boli.Color);
+            Boligrafos.Add(Boli);
+            Boli = new Boligrafo();//para borrar los campos, para ello el modelo debe ser observable
+        }
 
+    }
+    [RelayCommand]
+    public void MostrarOpcionesAvanzadas()
+    {
+        if (Avanzadas)
+        {
+            Avanzadas = false;
+        }
+        else
+        {
+            Avanzadas = true;
+        }
     }
 
 }
